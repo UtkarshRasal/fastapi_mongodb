48abe0f17424783387752fd3a4cced33cd06cd4f
utkarshrasal <utkarshrasal@gmail.com>
2022-05-03 18:29:23 +0530

    mongodb

--- config/database.py
+++ config/database.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-from dotenv import dotenv_values
 
-
-config = dotenv_values(".env")
+#connect to the mongo client
 client = MongoClient("mongodb+srv://utkarshrasal12:goodwish12@cluster0.n2908.mongodb.net/mongo_database?retryWrites=true&w=majority")
 
+#connect to the database.....(the database will automatically be created for the name you give)
 db = client.mongo_database
 
+#connect to the collection.....(the collection will automatically be created for the name you give)
 collection_name = db['mongo_collection']
 
 
--- routes/todos_routes.py
+++ routes/todos_routes.py
@@ -5,23 +5,38 @@ from config.database import collection_name
 
 from schemas.todos_schemas import todo_serializer, todos_serializer
 
-from bson import objectid
+from bson import ObjectId
 
 
 api_router = APIRouter()
 
-
+#post
 @api_router.post('/')
 async def create_todo(todo: Todo):
-    _id = collection_name.insert_one(dict(todo))
-    _todo = todos_serializer(collection_name.find({"_id": _id.inserted_id}))
+    id = collection_name.insert_one(dict(todo))
+    _todo = todos_serializer(collection_name.find({"id": id.inserted_id}))
 
     return {
         'data': todo
     }
 
+#retrieve
 @api_router.get('/')
 async def get_todos():
     todos = todos_serializer(collection_name.find())
 
     return todos
+
+# update
+@api_router.put("/{id}")
+async def update_todo(id: str, todo: Todo):
+    collection_name.find_one_and_update({"_id": ObjectId(id)}, {
+        "$set": dict(todo)
+    })
+    return todos_serializer(collection_name.find({"_id": ObjectId(id)}))
+
+# delete
+@api_router.delete("/{id}")
+async def delete_todo(id: str):
+    collection_name.find_one_and_delete({"_id": ObjectId(id)})
+    return {"status": "ok"}
